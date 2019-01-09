e944a1e939987e81667eea28318c3b0f77445f01
Jorge Zapata Guridi <jorge.guridi@hulu.com>
2019-01-09 13:51:35 -0600

    Addressed Sean's feedback

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 const testRunner = require('../src/');
 const argv = require('yargs')
-    .usage("Usage: $0 <option> test file")
-    .describe("f", "Path to test file")
-    .alias("f", "file")
+    .usage("Usage: $0 [command] [options]")
+    .updateStrings({
+        "Positionals:": "Available commands",
+        "Options:": "Other Options"
+    })
+    .command(
+        "file <file>",
+        "Run tests on given file", 
+        (yargs) => {
+            yargs.positional("file", {
+                type: "string",
+                describe: "The test file to execute"
+            })
+        },
+        (argv) => {
+            testRunner(argv.file);
+        }
+    )
     .help("h")
     .alias("h", "help")
     .argv;
 
-if (argv.f) {
-    testRunner(argv.f);
-} else {
-    console.warn("You need to specify a file to test.");
-}
