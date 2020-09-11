251b573e78f7091176733d78e1322bf4790de80b
Levi Kipke <levi.kipke@hulu.com>
2020-09-11 13:57:06 -0700

    update comment

--- src/coverage.js
+++ src/coverage.js
@@ -3,9 +3,8 @@ const Reports = require("istanbul-reports");
 const LibCoverage = require("istanbul-lib-coverage");
 const brs = require("brs");
 
-/**
- * Takes a formatted dictionary of file coverage and generates
- * given istanbul reports. Format needed for istanbul:
+/*
+ * Generates coverage reports using the given list of reporters.
  * @param {array} reporters the istanbul reporters to use
  */
 module.exports.report = function(reporters) {
