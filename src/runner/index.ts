8b06018f7272ec748a6b4d987b4d93ceed4e88b1
Levi Kipke <levi.kipke@hulu.com>
2021-01-25 14:21:42 -0800

    remove jest reference

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,27 +1,27 @@
 import TapMochaReporter = require("tap-mocha-reporter");
 import { TestRunner } from "./TestRunner";
 
-export type MochaReporterType =
-    | "classic"
-    | "doc"
-    | "dot"
-    | "dump"
-    | "json"
-    | "jsonstream"
-    | "landing"
-    | "list"
-    | "markdown"
-    | "min"
-    | "nyan"
-    | "progress"
-    | "silent"
-    | "spec"
-    | "tap"
-    | "xunit";
-export type JestReporterType = "jest" | "jest-verbose";
-export type ReporterType = JestReporterType | MochaReporterType;
+const mochaReporterTypes = [
+    "classic",
+    "doc",
+    "dot",
+    "dump",
+    "json",
+    "jsonstream",
+    "landing",
+    "list",
+    "markdown",
+    "min",
+    "nyan",
+    "progress",
+    "silent",
+    "spec",
+    "tap",
+    "xunit",
+] as const;
+export type MochaReporterType = typeof mochaReporterTypes[number];
 
-export function createTestRunner(reporterType: ReporterType) {
+export function createTestRunner(reporterType: MochaReporterType) {
     // Add more runner options as they are implemented
     return new TestRunner(new TapMochaReporter(reporterType));
 }
