965a3a0a62b68c0ea2944384f47ee340e39f64d3
Levi Kipke <levi.kipke@hulu.com>
2021-03-29 13:50:38 -0700

    remove transform

--- jest.config.js
+++ jest.config.js
@@ -168,9 +168,6 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     // transform: undefined,
-    //   transform: {
-    //     '.(ts|tsx|js|jsx)': 'ts-jest',
-    //   },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
