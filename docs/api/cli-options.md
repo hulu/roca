71e98c71e28befb6b0ad732423af03766141d18f
Levi Kipke <levi.kipke@hulu.com>
2021-04-20 13:14:53 -0700

    feat(cli): support CLI file matching (#91)

--- docs/api/cli-options.md
+++ docs/api/cli-options.md
@@ -1,14 +1,26 @@
-# `roca` CLI Options
+# `roca` CLI
+
+## Reporting
 
 Roca exclusively reports its state via the [Test Anything Protocol](http://testanything.org/), and defaults to a Mocha-like "spec" output.  Failed tests cause the `roca` CLI to return a non-zero exit code, which allows most continuous integration systems to automatically detect pass/fail states.
 
 Other output formats are available!  See `roca --help` for more details.
 
+## Usage
+
+```shell
+roca [options] [file names and/or patterns]
+```
+
+For more information, see [Running from the command line](getting-started/quick-start#running-from-the-command-line).
+
+## Options
+
 | Option                  | Description       |
 | ------------------------|----------------|
 | `-h`/`--help`           | The help menu. |
 | `-s`/`--source`         | Path to brs files (if different from `source/`) |
-| `-R`/`--reporter`       | The mocha reporter to use. See [the `mocha` docs](https://mochajs.org/#reporters) for a full list of reporters and example output. _Note: we use the [`tap-mocha-reporter`](https://github.com/tapjs/tap-mocha-reporter) NPM package for reporting._  |
+| `-R`/`--reporter`       | The reporter to use. In addition to all of [the `mocha` reporters](https://mochajs.org/#reporters), we also offer a [Jest-style reporter](https://jestjs.io/). _Note: we use the [`tap-mocha-reporter`](https://github.com/tapjs/tap-mocha-reporter) NPM package for Mocha reporters._  |
 | `-r`/`--require` | Path to a required setup file. This file will be run before unit tests. See [the `--require` section](#-r-require) for more details.|
 | `-f`/`--forbid-focused` | Fails if focused test or suite is detected. Useful for use on CI systems to prevent focused tests from being merged. |
 | `-c`/`--coverage-reporters` | The `istanbul` coverage reporters to use. Passing in reporter(s) will enable coverage collection and reporting. Otherwise, it is disabled. See `--help` for options, and [`istanbul`'s docs](https://istanbul.js.org/docs/advanced/alternative-reporters/) for descriptions of the reporters. |
---
+++ test/runner/file-match.test.ts
@@ -0,0 +1,81 @@
+import { globMatchFiles } from "../../src/util";
+
+let wrappedGlobMatchFiles = async (filePatterns: string[]) => {
+    let results = await globMatchFiles(filePatterns);
+    return results.map((result) => result.split(__dirname + "/resources/")[1]);
+};
+
+describe("globMatchFiles", () => {
+    let spy: jest.SpyInstance;
+
+    beforeEach(() => {
+        spy = jest.spyOn(process, "cwd");
+        spy.mockReturnValue(__dirname + "/resources");
+    });
+
+    afterEach(() => {
+        spy.mockRestore();
+    });
+
+    it("Finds all .test.brs files when not given patterns", async () => {
+        let results = await wrappedGlobMatchFiles([]);
+        expect(results).toEqual([
+            "fly-you-fools.test.brs",
+            "bar/bar-suffix.test.brs",
+            "bar/bar.test.brs",
+            "bar/prefix-bar.test.brs",
+            "foo/main.test.brs",
+            "foo2/main.test.brs",
+        ]);
+    });
+
+    it("Partially matches on filename suffix when given a .brs extension", async () => {
+        let results = await wrappedGlobMatchFiles(["bar.test.brs"]);
+        expect(results).toEqual([
+            "bar/bar.test.brs",
+            "bar/prefix-bar.test.brs",
+        ]);
+    });
+
+    it("Can handle a full path to a file", async () => {
+        let results = await wrappedGlobMatchFiles(["bar/bar.test.brs"]);
+        expect(results).toEqual(["bar/bar.test.brs"]);
+    });
+
+    it("Can handle asterisks in pattern", async () => {
+        let results = await wrappedGlobMatchFiles(["bar*.test.brs"]);
+        expect(results).toEqual([
+            "bar/bar-suffix.test.brs",
+            "bar/bar.test.brs",
+            "bar/prefix-bar.test.brs",
+        ]);
+    });
+
+    it("Looks for both partial directory and file matches when not given a .brs extension", async () => {
+        let results = await wrappedGlobMatchFiles(["foo"]);
+        expect(results).toEqual([
+            "fly-you-fools.test.brs",
+            "foo/main.test.brs",
+            "foo2/main.test.brs",
+        ]);
+    });
+
+    it("Excludes files when handling directories", async () => {
+        let results = await wrappedGlobMatchFiles(["foo/"]);
+        expect(results).toEqual(["foo/main.test.brs"]);
+    });
+
+    it("Handles a .brs file without .test if explicitly told to", async () => {
+        let results = await wrappedGlobMatchFiles(["no-test-ext.brs"]);
+        expect(results).toEqual(["no-test-ext.brs"]);
+    });
+
+    it("Handles multiple patterns", async () => {
+        let results = await wrappedGlobMatchFiles(["main", "prefix"]);
+        expect(results).toEqual([
+            "bar/prefix-bar.test.brs",
+            "foo/main.test.brs",
+            "foo2/main.test.brs",
+        ]);
+    });
+});
